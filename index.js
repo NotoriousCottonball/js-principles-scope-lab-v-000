105c9f269ddc3706db6e9355ba6fb2e0562165d7
George  Pianka <gpianka1@gmail.com>
2019-04-10 04:33:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 
 
- var customerName = "bob"; // global scope
+ var customerName = "bob";
 
  function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
